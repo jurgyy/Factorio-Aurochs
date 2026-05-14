6a428ec10401f88d694004e3d2ef28146d6521ad
Jurgen <jurgen.baas@outlook.com>
2026-05-14 23:07:30 +0200

    Move ATL code to separate file

---
+++ prototypes/animated-trains.lua
@@ -0,0 +1,43 @@
+data:extend{{
+  type = "mod-data",
+  name = "atl-config-aurochs",
+  data_type = "atl-config",
+  data = {
+    name = "aurochs-locomotive",
+    layers = {
+      {
+        file_path = "__aurochs__/graphics/loco/BullDraft",
+        draw_as_shadow = false,
+        spritter_table = require("graphics/loco/BullDraft"),
+        flags = { "low-object" }
+      },
+      {
+        file_path = "__aurochs__/graphics/loco/BullDraftShadow",
+        draw_as_shadow = true,
+        spritter_table = require("graphics/loco/BullDraftShadow"),
+      }
+    },
+    animation_speed_multiplier = 16.5
+  }
+}, {
+  type = "mod-data",
+  name = "atl-config-aurochs-cart",
+  data_type = "atl-config",
+  data = {
+    name = "aurochs-cart",
+    layers = {
+      {
+        file_path = "__aurochs__/graphics/wagon/Object",
+        draw_as_shadow = false,
+        spritter_table = require("graphics/wagon/Object"),
+        flags = { "low-object" }
+      },
+      {
+        file_path = "__aurochs__/graphics/wagon/Shadow",
+        draw_as_shadow = true,
+        spritter_table = require("graphics/wagon/Shadow"),
+      }
+    },
+    animation_speed_multiplier = 6
+  }
+}}
